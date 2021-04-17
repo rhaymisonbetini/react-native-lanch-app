329e21ca2977b7a1bd8949899718e4e79bd24517
RHAYMISON <rhaymison@hotmail.com>
2021-04-17 11:29:14 -0300

    categories

--- screens/Home.js
+++ screens/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
     SafeAreaView,
@@ -16,68 +16,138 @@ import { HomeStyles } from './styles/HomeStyles';
 //database
 import { restaurantData, categoryData, initialCurrentLocation } from '../database/HomeDataBase';
 
-const renderHeader = () => {
+
+
+const Home = () => {
 
     const [categories, setCategories] = React.useState(categoryData)
     const [selectedCategory, setSelectedCategory] = React.useState(null)
     const [restaurants, setRestaurants] = React.useState(restaurantData)
     const [currentLocation, setCurrentLocation] = React.useState(initialCurrentLocation)
 
-    return (
-        <View style={{ flexDirection: 'row', justifyContent: 'center', height: 50, marginTop: 30 }}>
-            <TouchableOpacity
-                style={{
-                    width: 50,
-                    paddingLeft: SIZES.padding * 1,
-                    justifyContent: 'center'
-                }}>
-                <Image
-                    source={icons.nearby}
-                    resizeMode="contain"
+    const renderHeader = () => {
+
+        return (
+            <View style={{ flexDirection: 'row', justifyContent: 'center', height: 50, marginTop: 30 }}>
+                <TouchableOpacity
                     style={{
-                        width: 25,
-                        height: 25,
-                    }}
-                />
-            </TouchableOpacity>
-            <View style={{ flex: 1, alignContent: 'center', justifyContent: 'center' }}>
-                <View style={{
-                    width: '100%',
-                    height: '100%',
-                    backgroundColor: COLORS.lightGray3,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    borderRadius: SIZES.radius
-                }}>
-                    <Text>Localização atual</Text>
+                        width: 50,
+                        paddingLeft: SIZES.padding * 1,
+                        justifyContent: 'center'
+                    }}>
+                    <Image
+                        source={icons.nearby}
+                        resizeMode="contain"
+                        style={{
+                            width: 25,
+                            height: 25,
+                        }}
+                    />
+                </TouchableOpacity>
+                <View style={{ flex: 1, alignContent: 'center', justifyContent: 'center' }}>
+                    <View style={{
+                        width: '100%',
+                        height: '100%',
+                        backgroundColor: COLORS.lightGray3,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        borderRadius: SIZES.radius
+                    }}>
+                        <Text>{initialCurrentLocation.streetName}</Text>
+                    </View>
                 </View>
-            </View>
-            <TouchableOpacity
-                style={{
-                    width: 50,
-                    paddingLeft: SIZES.padding * 1,
-                    justifyContent: 'center'
-                }}>
-                <Image
-                    source={icons.basket}
-                    resizeMode="contain"
+                <TouchableOpacity
                     style={{
-                        width: 25,
-                        height: 25,
+                        width: 50,
+                        paddingLeft: SIZES.padding * 1,
+                        justifyContent: 'center'
+                    }}>
+                    <Image
+                        source={icons.basket}
+                        resizeMode="contain"
+                        style={{
+                            width: 25,
+                            height: 25,
+                        }}
+                    />
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+    const onSelectItem = (item) => {
+        let restaurantList = restaurantData.filter(restaurant => restaurant.categories.includes(item.id))
+        setRestaurants(restaurantList);
+        setSelectedCategory(item)
+    }
+
+    function renderMainCategories() {
+
+        const renderItem = ({ item }) => {
+            return (
+                <TouchableOpacity
+                    style={{
+                        padding: SIZES.padding,
+                        paddingBottom: SIZES.padding * 2,
+                        backgroundColor:  COLORS.primary,
+                        borderRadius: SIZES.radius,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        marginRight: SIZES.padding,
+                        ...HomeStyles.shadow
                     }}
+                    onPress={() => onSelectItem(item)}
+                >
+                    <View style={{
+                        width: 50,
+                        height: 50,
+                        borderRadius: 25,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        backgroundColor: COLORS.white
+                    }}>
+                        <Image
+                            source={item.icon}
+                            resizeMode="contain"
+                            style={{
+                                width: 30,
+                                height: 30
+                            }}
+                        />
+                    </View>
+                    <Text style={{
+                        marginTop: SIZES.padding,
+                        color: COLORS.white,
+                        ...FONTS.body5
+                    }}>
+                        {item.name}
+                    </Text>
+
+                </TouchableOpacity>
+            )
+        }
+
+        return (
+            <View style={{ padding: SIZES.padding * 2 }}>
+                <Text style={{ ...FONTS.h1 }}>Os</Text>
+                <Text style={{ ...FONTS.h1 }}>mais pedidos</Text>
+                <FlatList
+                    data={categories}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    keyExtractor={item => `${item.id}`}
+                    renderItem={renderItem}
+                    contentContainerStyle={{ padding: SIZES.padding * 2 }}
+
                 />
-            </TouchableOpacity>
-        </View>
-    )
-}
+            </View>
+        )
+    }
 
-
-
-
-const Home = () => {
     return (
         <SafeAreaView style={HomeStyles.container}>
             {renderHeader()}
+            {renderMainCategories()}
         </SafeAreaView>
     )
 }
